d6361d1bc6048b18e20081e87df5a4b63bfa5cd4
ZahidHR99 <zahidhr99@gmail.com>
2024-01-01 09:31:19 +0600

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import { useState } from "react";
+import { useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+ 
+  useMemo(()=>{
+    getData();
+  },[])
+
+  function getData(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setData(json))
+  }
+
+  console.log(data)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>React - Redux</h1>
+
+     {
+      data?.map((row)=>(
+        <>
+          <h2>{row.title}</h2>
+          <p>{row.body}</p>
+        </>
+      ))
+     }
+
     </>
   )
 }

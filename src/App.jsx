fba24c508d1de36e2a2bf1f3083a37458b820e46
ZahidHR99 <zahidhr99@gmail.com>
2024-01-02 09:40:30 +0600

    react-helmete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import EffectUse from "./components/EffectUse"
 import CountUse from "./components/CountUse"
 import MemoUse from "./components/MemoUse"
+import {Helmet} from "react-helmet";
 
 function App() {
 
   return (
     <>
-     <h1>React - Redux</h1>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home-React Redux</title>
+        <meta name="description" content="Helmet application" />
+    </Helmet>
+
+    <h1>React - Redux</h1>
 
     <MemoUse/>
 

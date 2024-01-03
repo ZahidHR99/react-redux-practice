2cf20410538b5512af242eda565ef8a74121a9d9
ZahidHR99 <zahidhr99@gmail.com>
2024-01-03 10:17:46 +0600

    react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import CountUse from "./components/CountUse"
 import MemoUse from "./components/MemoUse"
 import {Helmet} from "react-helmet";
 import AxiosComponent from "./components/AxiosComponent";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => toast.success("Wow so easy!",{
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
 
   return (
     <>
@@ -13,6 +25,10 @@ function App() {
         <title>Home-React Redux</title>
         <meta name="description" content="Helmet application" />
     </Helmet>
+
+    <button onClick={notify}>Notify!</button>
+
+    <ToastContainer />
 
     <h1>React - Redux -Axios</h1>
 
